29f8db7a9c69cd4bf90c8bb75a036b3c29d159c6
Bjorn <brendoandrade01@gmail.com>
2026-06-08 14:19:49 -0300

    add Tnet framework option to config

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ BridgeConfig = {
 
     ["UpdateCheck"] = true,     ----- Enable update check.
     ["Language"] = "br",        ----- br | en | es
-    ["Framework"] = "vrpex",    ----- vrp | vrpex | creativev5 | creativev6 | creativeNetwork | creativeEnchanted | esx | qbcore | qbox
+    ["Framework"] = "vrpex",    ----- vrp | vrpex | creativev5 | creativev6 | creativeNetwork | creativeEnchanted | esx | qbcore | qbox | tnet
     ["Inventory"] = "default",  ----- default | ox_inventory
 
     ["Keys"] = {
